576a7a1121a5274ce6a5b98d62addb59055b366e
Nishant Gupta <gupta.nishant08042004@gmail.com>
2025-08-08 01:07:39 +0530

    Update pricing.tsx

--- components/frontend/pricing.tsx
+++ components/frontend/pricing.tsx
@@ -14,7 +14,7 @@ export default function Pricing() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="">
-            <SectionHeader title="✨Pricing Plans" description="Affordable school management solution designed to streamline your 
+            <SectionHeader title="✨ Pricing Plans" description="Affordable school management solution designed to streamline your 
             educational operations. No hidden fees, no per-student charges - 
             just one simple price for unlimited access." heading="Transparent pricing for schools" />
         </div>
